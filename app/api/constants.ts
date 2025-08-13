d189793ae2ed69c6ddb936275671d86675f6d9ae
Purson <purson.chen@gmail.com>
2025-08-13 09:15:38 +0800

    replace testnet3 to testnet2

--- app/api/constants.ts
+++ app/api/constants.ts
@@ -6,12 +6,17 @@ export const PERMISSION = process.env.PERMISSION ?? 'active';
 export const BROADCAST = Boolean(process.env.BROADCAST ?? 'true');
 
 export const CHAINS = {
+  testnet2: {
+    id: "20d14b6e421fe21ef1f39061e70f214d5d908b979f379a4feea04294944ddd92",
+    url: "https://chain2.exactsat.io/",
+    explorer: "https://scan2.exactsat.io/",
+  },
   testnet3: {
     id: "20d14b6e421fe21ef1f39061e70f214d5d908b979f379a4feea04294944ddd92",
     url: "https://chain-tst3.exsat.network/",
     explorer: "https://scan-testnet.exsat.network/",
   },
 };
-export const CHAIN_DEFAULT = "testnet3";
+export const CHAIN_DEFAULT = "testnet2";
 export const CHAIN_ID = CHAINS[CHAIN_DEFAULT].id;
 export const CHAIN_URL = CHAINS[CHAIN_DEFAULT].url;
